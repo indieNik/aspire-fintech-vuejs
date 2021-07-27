d0abae1c2be528f05043df8ed4fcddeed6e8f208
Nikhil Patil <nikhil@openapp.co>
2021-07-27 19:25:52 +0530

    Completed Home View

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,31 @@
 <template>
-  <div class="home">
+  <div class="home mb-20">
     <section class="welcome">
       <h1 class="text-2xl md:text-4xl font-bold mb-2">Welcome, Mark.</h1>
       <p class="text-sm tracking-widest">What would you like to get done today?</p>
     </section>
-    <div class="limits my-10">
-      <div class="flex items-center justify-between">
-        <h2 class="text-lg md:text-xl font-bold">
-          Your credit limits
-        </h2>
-        <router-link class="text-primary" to="/cards">View all</router-link>
+    <div class="grid md:grid-cols-2 md:gap-8">
+      <div class="credit-limits my-10">
+        <div class="flex items-center justify-between">
+          <h2 class="text-lg md:text-xl font-bold">
+            Your credit limits
+          </h2>
+          <router-link class="text-primary" to="/cards">View all</router-link>
+        </div>
+        <p class="text-sm text-gray-400 tracking-wider">Go to the cards tab to view details</p>
+        <div class="collapsible-wrapper border border-gray-50 shadow-xl rounded-xl overflow-hidden h-64 mt-4">
+        </div>
       </div>
-      <p class="text-sm text-gray-400 tracking-wider">Go to the cards tab to view details</p>
+      <div class="credit-limits my-10">
+        <div class="flex items-center justify-between">
+          <h2 class="text-lg md:text-xl font-bold">
+            Spend Breakdown
+          </h2>
+        </div>
+        <p class="text-sm text-gray-400 tracking-wider">30-day card spend breakdown</p>
+        <div class="collapsible-wrapper border border-gray-50 shadow-xl rounded-xl overflow-hidden h-64 mt-4">
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -19,7 +33,6 @@
 <script>
 export default {
   name: 'Home',
-  components: {
-  }
+  components: []
 }
 </script>
